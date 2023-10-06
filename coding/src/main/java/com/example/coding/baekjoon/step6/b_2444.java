ffddeddd587663c1b18ee176cb6f27bbcedc2478
jh.ku <jh.ku@konai.com>
2023-10-06 17:26:31 +0900

    commit

--- coding/src/main/java/com/example/coding/baekjoon/step6/b_2444.java
+++ coding/src/main/java/com/example/coding/baekjoon/step6/b_2444.java
@@ -10,7 +10,7 @@ public class b_2444 {
         int n = Integer.parseInt(br.readLine());
 
         for(int i = 1; i <= n; i++) {
-            for(int j = n - i; j >= 0; j--)
+            for(int j = n - i; j >= 1; j--)
                 bw.write(" ");
             for(int k = 1; k <= i * 2 - 1; k++)
                 bw.write("*");
@@ -18,13 +18,25 @@ public class b_2444 {
         }
 
         for(int i = 1; i <= n - 1; i++) {
-            for(int j = 1; j <= i + 1; j++)
+            for(int j = 1; j <= i; j++)
                 bw.write(" ");
-            for(int k = i; k <= i + n; k++)
+            for(int k = 1; k <= n * 2 - (i * 2 + 1); k++)
                 bw.write("*");
             bw.write("\n");
         }
 
+        /**
+         *  입력 : 5
+         *  결과 : 7 5 3 1 이 나와야 함
+         *  가공은 1 2 3 4
+         *  더하면 6 7 8 9
+         *  뻬면  4 3 2 1
+         *  두배 하면 2 4 6 8
+         *  가공을 4 3 2 1
+         *  3 5 7 9
+         *  1 3 5 7 9
+         *  10 에서 3 5 7 9  빼는 방햐응로
+         * */
         bw.flush();
         bw.close();
         br.close();
