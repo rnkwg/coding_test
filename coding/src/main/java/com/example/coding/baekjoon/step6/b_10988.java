2b0defab5784203cd25c2e1e14f27e3a48d545ee
jh.ku <jh.ku@konai.com>
2023-10-10 16:41:47 +0900

    commit

---
+++ coding/src/main/java/com/example/coding/baekjoon/step6/b_10988.java
@@ -0,0 +1,28 @@
+package com.example.coding.baekjoon.step6;
+
+import java.io.*;
+
+public class b_10988 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+
+        String[] s = br.readLine().split("");
+        boolean flag = true;
+
+        for(int i = 0; i < s.length / 2; i++) {
+            if(!s[i].equals(s[s.length - i - 1])) {
+                bw.write("0");
+                flag = false;
+                break;
+            }
+        }
+
+        if(flag)
+            bw.write("1");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
