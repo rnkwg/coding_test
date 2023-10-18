c074278b36ec4494152f8ff886241bc3defe0f70
jh.ku <jh.ku@konai.com>
2023-10-18 17:26:14 +0900

    commit

--- coding/src/main/java/com/example/coding/자료구조/b_11659.java
+++ coding/src/main/java/com/example/coding/자료구조/b_11659.java
@@ -10,30 +10,49 @@ public class b_11659 {
         /**
          * 입력
          * 5 3
-         * 5 4 3 2 1
-         * 1 3
-         * 2 4
-         * 5 5
+         * 5 4 3 2 1 : array
+         * 5 9 12 14 15 : sum
+         * 5 9 7 5 3
+         *
+         * 1 3 : 12
+         * 2 4 : 9
+         * 5 5 : 1
          *
          * 출력
          * 12
-         * 9
-         * 1
+         * 9 = sum[3] - array[0]
+         * 1 = sum[4] - array[4]
          */
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
         int[] array = new int[n];
-        int[] sum = new int[m];
+        int[] sum = new int[n];
+        int[] answer = new int[m];
+
+        st = new StringTokenizer(br.readLine(), " ");
         for(int i = 0; i < n; i++) {
-            StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
-            array[i] = Integer.parseInt(st1.nextToken());
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+        sum[0] = array[0];
+        for(int i = 1; i < n; i++) {
+            sum[i] = sum[i - 1] + array[i];
         }
 
         for(int i = 0; i < m; i++) {
-            StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
-            array[i] = Integer.parseInt(st1.nextToken());
+            st = new StringTokenizer(br.readLine(), " ");
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            if(start > 1)
+                answer[i] = sum[end - 1] - sum[start - 1];
+            else
+                answer[i] = sum[end - 1];
+        }
+
+        for(int i = 0; i < m; i++) {
+            bw.write(answer[i] + "\n");
         }
 
         bw.flush();
