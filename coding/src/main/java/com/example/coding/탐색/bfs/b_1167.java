49f0cc540e6348582198630cf6cca3944f92ba61
jh.ku <jh.ku@konai.com>
2024-03-14 22:28:09 +0900

    commit

--- coding/src/main/java/com/example/coding/탐색/bfs/b_1167.java
+++ coding/src/main/java/com/example/coding/탐색/bfs/b_1167.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class b_2178 {
+public class b_1167 {
     static boolean visited[][];
     static Integer[][] graph;
     static Integer[][] distance;
