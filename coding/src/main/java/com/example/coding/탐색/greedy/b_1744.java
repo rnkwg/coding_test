cdf124f30f123d626965695c33d1e2eedea1ade1
jh.ku <jh.ku@konai.com>
2024-05-08 15:53:17 +0900

    commit

---
+++ coding/src/main/java/com/example/coding/탐색/greedy/b_1744.java
@@ -0,0 +1,66 @@
+package com.example.coding.탐색.greedy;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class b_1744 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int length = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i = 0; i < length; i++) {
+            queue.add(Integer.parseInt(br.readLine()));
+        }
+
+        int sum = 0;
+        while(queue.size() > 1) {
+            int num1 = queue.poll();
+            int num2 = queue.poll();
+
+            // (+, +)
+            // (+, 0)
+            // (0, 0)
+            // (0, -)
+            // (-, -)
+            if(num1 > 0 && num2 > 0) {
+                if(num1 * num2 > num1 + num2) {
+                    sum += (num1) * (num2);
+                } else {
+                    sum += (num1) + (num2);
+                }
+            } else if(num1 > 0 && num2 == 0) {
+                if(num1 * num2 > num1 + num2) {
+                    sum += (num1) * (num2);
+                } else {
+                    sum += (num1) + (num2);
+                }
+            } else if(num1 == 0 && num2 == 0) {
+                sum += (num1) * (num2);
+            } else if(num1 == 0 && num2 < 0){
+                if(num1 * num2 > num1 + num2) {
+                    sum += (num1) * (num2);
+                } else {
+                    sum += (num1) + (num2);
+                }
+            } else if(num1 < 0 && num2 < 0) {
+                if(num1 * num2 > num1 + num2) {
+                    sum += (num1) * (num2);
+                } else {
+                    sum += (num1) + (num2);
+                }
+            }
+        }
+
+        if(!queue.isEmpty())
+            sum += queue.poll();
+
+        bw.write(sum + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
---
+++ coding/src/main/java/com/example/coding/탐색/greedy/b_1931.java
@@ -0,0 +1,49 @@
+package com.example.coding.탐색.greedy;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class b_1931 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int length = Integer.parseInt(br.readLine());
+        int[][] timeTable = new int[length][2];
+
+        for(int i = 0; i < length; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            timeTable[i][0] = Integer.parseInt(st.nextToken());
+            timeTable[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        // 회의시간이 일찍 끝나는 순서대로 정렬
+        Arrays.sort(timeTable, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+        });
+
+        // 끝 시간 <= 시작시간 이면
+        // count + 1, 끝 시간 다시 할당해주기
+        int count = 0;
+        int end = -1;
+        for(int i = 0; i < length; i++) {
+            if(timeTable[i][0] >= end) {
+                end = timeTable[i][1];
+                count++;
+            }
+        }
+
+        bw.write(count + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
